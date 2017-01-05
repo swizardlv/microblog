c3e1d16a257b5a45f6f507d19f4ad8230de9035b
swizardlv <swizardlv@gmail.com>
2017-01-05 20:22:58 +0800

    change folder

--- app/views.py
+++ app/views.py
@@ -6,4 +6,4 @@ from app import app
 def index():
     """index function"""
     user = {'nickname' : 'mike'}
-    return render_template("./template/index.html", title='Home', user=user)
+    return render_template("index.html", title='Home', user=user)

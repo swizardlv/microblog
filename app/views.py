1ec5cf0956144368606e11c9e43363e0b666b83a
swizardlv <swizardlv@gmail.com>
2017-01-05 20:42:29 +0800

    add for in template

--- app/views.py
+++ app/views.py
@@ -1,9 +1,16 @@
 """index function"""
 from flask import render_template
 from app import app
+
+
 @app.route('/')
 @app.route('/index')
 def index():
     """index function"""
-    user = {'nickname' : 'mike'}
-    return render_template("index.html", title='Home', user=user)
+    user = {'nickname': 'mike'}
+    posts = [
+        {'author': {'nickname': 'steve'},
+         'body': 'i am cool'},
+        {'author': {'nickname': 'trump'},
+         'body': 'i am rich'}]
+    return render_template("index.html", title='Home', user=user, posts=posts)

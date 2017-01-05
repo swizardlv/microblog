c83ed84bfcc6a68140bec2d63d982ac8251568e2
swizardlv <swizardlv@gmail.com>
2017-01-05 20:11:48 +0800

    init

---
+++ app/views.py
@@ -0,0 +1,9 @@
+"""index function"""
+from flask import render_template
+from app import app
+@app.route('/')
+@app.route('/index')
+def index():
+    """index function"""
+    user = {'nickname' : 'mike'}
+    return render_template("./template/index.html", title='Home', user=user)
